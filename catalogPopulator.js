4f40a480073a110aa96f6a096c1c619109dc7e8e
gabrielkhabibulin <gaba93@yandex.ru>
2022-11-09 15:46:55 +0300

    catalog populator

--- catalogPopulator.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-const citiesListData = require('./src/mocks/mocks.ts');
-
-for (let i = 0; i <= 100; i++) {
-  const randomCity = citiesListData[Math.floor(Math.random() * citiesListData.length)];
-  const carObj = JSON.stringify({ id: i, city: randomCity });
-  fs.writeFileSync('./cars.json', carObj);
-}
---
+++ src/catalogPopulator.js
@@ -0,0 +1,13 @@
+import fs from 'fs';
+import { citiesListData, carBrands } from './mocks/mocks.js';
+
+const carEntityArr = [];
+
+for (let i = 0; i <= 100; i++) {
+  const randomCity = citiesListData[Math.floor(Math.random() * citiesListData.length)];
+  const randomCar = carBrands[Math.floor(Math.random() * carBrands.length)];
+  const carObj = { id: i, city: randomCity, brand: randomCar };
+  carEntityArr.push(carObj);
+}
+
+fs.writeFileSync('./cars.json', JSON.stringify(carEntityArr));
