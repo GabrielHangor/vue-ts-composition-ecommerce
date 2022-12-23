b0e83b3c792818eb8f3636f5b3344d884f24fed2
Gabriel <gaba93@yandex.ru>
2022-12-23 17:46:21 +0300

    Updates

--- assets/AboutPage-822361c9.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c as o}from"./index-8079d185.js";const c={},n={class:"max-h-full w-full text-center text-5xl font-bold text-orange-base md:text-9xl"};function s(a,r){return t(),o("div",n," ABOUT PAGE ")}const _=e(c,[["render",s]]);export{_ as default};
