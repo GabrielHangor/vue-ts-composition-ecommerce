68f27d488a5190b7dbfd0a878284961b07d5ab57
Gabriel <gaba93@yandex.ru>
2022-12-27 17:59:13 +0300

    Updates

--- assets/DetailsPage-c8e81e3a.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c}from"./index-00755016.js";const o={},s={class:"w-full max-h-full text-center text-5xl md:text-9xl font-bold text-orange-base"};function a(n,l){return t(),c("div",s,"DETAIL PAGE")}const _=e(o,[["render",a]]);export{_ as default};
