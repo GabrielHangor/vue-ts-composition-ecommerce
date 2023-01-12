bee99a2cdc6d84905b0252965e070d3a2198c11e
Gabriel <gaba93@yandex.ru>
2023-01-12 12:36:44 +0300

    FEATURE: filters correct display on mobile

--- src/components/OurCarsSection/OurCarsCatalogFilters.vue
+++ src/components/OurCarsSection/OurCarsCatalogFilters.vue
@@ -1,11 +1,11 @@
 <template>
   <aside
-    class="fixed top-0 left-0 h-full overflow-hidden px-1 transition-[width] duration-300 ease-in-out md:relative md:col-span-4 md:w-auto lg:col-span-3"
-    :class="isOpen ? 'w-full' : 'w-0'"
+    class="md:z-1 fixed top-0 left-0 z-10 flex h-full min-h-[100vh] w-full flex-col items-center overflow-hidden bg-white p-4 transition-[opacity] duration-300 ease-in md:pointer-events-auto md:relative md:relative md:col-span-4 md:w-auto md:p-1 md:opacity-100 lg:col-span-3"
+    :class="isOpen ? 'pointer-events-auto opacity-100' : 'pointer-events-none opacity-0'"
   >
     <img
       @click="$emit('closeMobileFilters')"
-      class="absolute right-0 p-4 md:hidden"
+      class="absolute top-0 right-0 p-4 md:hidden"
       src="/burger-closed.svg"
       alt="Burger close icon"
     />
